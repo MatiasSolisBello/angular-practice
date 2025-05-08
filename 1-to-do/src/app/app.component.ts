7f1e2d3bde026ca508d3824b271abe6186dc82b0
MatiasSolisBello <matias.solis.bello@gmail.com>
2025-05-08 15:17:22 -0400

    CRUD by localStorage

--- 1-to-do/src/app/app.component.ts
+++ 1-to-do/src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = '1-to-do';
+export class AppComponent implements OnInit {
+
+  tasksList:string[] = [];
+  newTask:string = '';
+
+  private _taskService =  inject(TaskService);
+
+  ngOnInit(): void {
+    console.log(this._taskService.getTasks());
+    this.tasksList = this._taskService.getTasks();
+  }
+
+  addTask() {
+    this._taskService.addTasks(this.newTask);
+    this.tasksList = this._taskService.getTasks();
+    this.newTask = '';
+    
+  }
+
+  deleteTask(index: number) {
+    this._taskService.deleteTask(index);
+    this.tasksList = this._taskService.getTasks();
+  }
+
+  
 }
--- 1-to-do/src/app/services/task.service.ts
+++ 1-to-do/src/app/services/task.service.ts
@@ -4,6 +4,24 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TaskService {
+  private localStorageKey = 'tasks';
 
-  constructor() { }
+  getTasks():string[]{
+    console.log(localStorage.getItem(this.localStorageKey));
+    return JSON.parse(localStorage.getItem(this.localStorageKey) as string) || []; 
+  }
+
+  addTasks(task: string){
+    const tasks = this.getTasks();
+    tasks.push(task);
+    localStorage.setItem(this.localStorageKey, JSON.stringify(tasks));
+  }
+
+
+  deleteTask(index: number){
+    const tasks = this.getTasks();
+    tasks.splice(index, 1);
+    localStorage.setItem(this.localStorageKey, JSON.stringify(tasks));
+  }
+
 }
