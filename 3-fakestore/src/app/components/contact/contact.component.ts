60c56b1b2109166f19cab177848220b6ee4b7516
MatiasSolisBello <matias.solis.bello@gmail.com>
2025-05-10 20:57:55 -0400

    Contact Component

--- 3-fakestore/src/app/components/contact/contact.component.ts
+++ 3-fakestore/src/app/components/contact/contact.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, NgClass],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+
+  contactForm!: FormGroup;
+  successMessage = '';
+
+  ngOnInit(): void {
+
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      full_name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    
+    })
+  }
+
+  send(event: Event) {
+    event.preventDefault();
+
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      const fullName = this.contactForm.get('full_name')?.value;
+      this.successMessage = `Mensaje de ${fullName} enviado exitosamente.`;
+      this.contactForm.reset(); // Opcional: limpia el formulario
+    }
+  }
+
+  hasErrors(field: string, typeError: string) {
+    return this.contactForm.get(field)?.hasError(typeError) && this.contactForm.get(field)?.touched;
+  }
 
 }
